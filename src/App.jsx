503c8afd3ef1733f3f6a533a6cc53886b7f9da67
JGRex-Joy <omurkulovamir2008@gmail.com>
2026-03-07 00:45:36 +0600

    добавил заглушку после реги и логина, чисто чекать работает ли

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import { useState } from "react";
 import WelcomPage from './components/WelcomePage/WelcomPage.jsx';
 import Register from './components/Register/Register.jsx';
 import LogIn from './components/LogIn/LogIn.jsx';
+import Home from './components/Home/Home.jsx';
 
 function App() {
   const [page, setPage] = useState("welcome");
 
   const handleNavigate = (p) => {
-    if (p === "back") setPage("welcome");  // ← назад = welcome
+    if (p === "back") setPage("welcome");
     else setPage(p);
   };
 
   return (
     <div>
-      {page === "welcome" && <WelcomPage onNavigate={handleNavigate} />}
-      {page === "register" && <Register onNavigate={handleNavigate} />}
-      {page === "login" && <LogIn onNavigate={handleNavigate} />}
+      {page === "welcome"  && <WelcomPage onNavigate={handleNavigate} />}
+      {page === "register" && <Register  onNavigate={handleNavigate} />}
+      {page === "login"    && <LogIn     onNavigate={handleNavigate} />}
+      {page === "home"     && <Home />}
     </div>
   );
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100dvh",
+      fontFamily: "Manrope, sans-serif",
+      fontSize: "24px",
+      fontWeight: "600",
+    }}>
+      Всё ок, работает 
+    </div>
+  );
+}
+
+export default Home;
